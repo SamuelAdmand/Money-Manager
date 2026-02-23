7efb16387ade2092891663f9948fb108876bd6fa
SamuelAdmand <snowden06062013@protonmail.com>
2026-02-24 00:11:21 +0530

    Add PWA assets and configure GitHub Pages deployment

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import './style.css';
 
+// --- Service Worker Registration ---
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('SW Registered!', reg))
+            .catch(err => console.log('SW registration failed: ', err));
+    });
+}
+
 // --- Interfaces ---
 interface Account {
     id: string;
